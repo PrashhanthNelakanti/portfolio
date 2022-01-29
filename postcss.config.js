f6b56376c3304c753aa33a0d603c0d7459b87202
prashhanthsoujanya <prashhanth.nelakanti@gmail.com>
2022-01-29 12:09:10 +0530

    intial cmt

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  darkMode: false, // or 'media' or 'class'
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
