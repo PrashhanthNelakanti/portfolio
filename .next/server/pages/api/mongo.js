3fe7a9a91585e8a6d97f54a56874bf852ead906c
prashhanthsoujanya <prashhanth.nelakanti@gmail.com>
2022-01-31 00:15:12 +0530

    CURD ops

---
+++ .next/server/pages/api/mongo.js
@@ -0,0 +1,44 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 816;
+exports.ids = [816];
+exports.modules = {
+
+/***/ 13:
+/***/ ((module) => {
+
+module.exports = require("mongodb");
+
+/***/ }),
+
+/***/ 516:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _lib_mongodb__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(57);
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (async (req, res)=>{
+    const { db  } = await (0,_lib_mongodb__WEBPACK_IMPORTED_MODULE_0__.connectToDatabase)();
+    const movies = await db.collection("studio").find({}).limit(20).toArray();
+    console.log(movies);
+    res.json(movies);
+});
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [57], () => (__webpack_exec__(516)));
+module.exports = __webpack_exports__;
+
+})();
--- pages/info.js
+++ pages/info.js
@@ -14,12 +14,11 @@ export default function Info({ data }) {
             ) : (
                 <ul>
                     {data.map((info, i) => (
-                        <div className="border-t border-gray-300">
-                            <div className="text-sm font-bold text-gray-800">SNo.{i}</div>
+                        <div className="border-t border-gray-300" key={info.id}>
                           <dl>
                             <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                               <dt className="text-sm font-medium text-gray-500">Full name</dt>
-                              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{info.fname}</dd>
+                              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2" >{info.fname}</dd>
                             </div>
                             <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                               <dt className="text-sm font-medium text-gray-500">Last name for</dt>
