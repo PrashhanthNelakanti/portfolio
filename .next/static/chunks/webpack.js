2d9be243ee507cbdb2a88cab73affae0b37818cb
prashhanthsoujanya <prashhanth.nelakanti@gmail.com>
2022-02-07 12:22:14 +0530

    Animation fade-right,left

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bbdf5d8bcffef3cf"; }
+/******/ 		__webpack_require__.h = function() { return "097569500e84e011"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
