103d1ebbb6efa737389ba6fd81f76010546b6537
prashhanthsoujanya <prashhanth.nelakanti@gmail.com>
2022-03-01 00:14:10 +0530

    otp and env

--- pages/api/mailSender.js
+++ pages/api/mailSender.js
@@ -1,13 +1,12 @@
 export default async function handler(req, res) {
-    console.log(await "In mailsnder")
     switch (req.method) {
         case 'POST': {
-            return addPost(req, res);
+            return sendOtp(req, res);
         }
     }
 }
 
-async function addPost(req, res) {
+async function sendOtp(req, res) {
     try {
         const nodemailer = require('nodemailer');
 
@@ -16,12 +15,12 @@ async function addPost(req, res) {
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: `${process.env.EMAIL}`,
-                pass: `${process.env.EMAIL_PWD}`
+                user: 'prashhanth.online@gmail.com',
+                pass: 'rock@1234A'
             }
         });
         var mailOptions = {
-            from: `${process.env.EMAIL}`,
+            from: 'prashhanth.online@gmail.com',
             to: body.email,
             subject: body.fname,
             html: `<h1>Here is the Otp</h1>`
