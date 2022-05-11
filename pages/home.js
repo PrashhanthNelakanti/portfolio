f2650961c824c350360fbde4f325570f40874e4f
prashhanthsoujanya <prashhanth.nelakanti@gmail.com>
2022-05-11 21:50:08 +0530

    fixed the home page consmetic error and added freelance profile

--- pages/home.js
+++ pages/home.js
@@ -17,7 +17,7 @@ const Home = () => {
             "In some ways, programming is like painting. You start with a blank canvas and certain basic raw materials. You use a combination of science, art, and craft to determine what to do with them." - Andrew Hunt
           </p>
           <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-            <div className="rounded-md shadow">
+            <div className="mt-3 sm:mt-0 sm:ml-3">
               <a
                 href="register"
                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
@@ -27,7 +27,7 @@ const Home = () => {
             </div>
             <div className="mt-3 sm:mt-0 sm:ml-3">
               <a
-                href="/profile_pdf/profile.pdf"
+                href="/profile_pdf/profile_free.pdf"
                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                download >
                 Get profile
