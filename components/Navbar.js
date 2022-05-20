be58d7931686f63b5b8903d8eef61ddb6fb06af4
prashhanthsoujanya <prashhanth.nelakanti@gmail.com>
2022-05-21 01:51:42 +0530

    login fixed in mobile and projects url fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -146,7 +146,7 @@ export default function Navbar() {
                             <Disclosure.Panel className="md:hidden">
                                 <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                                     {navigation.map((item) => (
-                                        <Disclosure.Button
+                                        item.name != 'Login' && loggedIn ? <Link href={item.href}>  <Disclosure.Button
                                             key={item.name}
                                             as="a"
                                             href={item.href}
@@ -158,8 +158,22 @@ export default function Navbar() {
                                         >
                                             {item.name}
                                         </Disclosure.Button>
+                                        </Link> : item.name == 'Login' && !loggedIn ?
+                                            <Link href={item.href}>  <Disclosure.Button
+                                                key={item.name}
+                                                as="a"
+                                                href={item.href}
+                                                className={classNames(
+                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                    'block px-3 py-2 rounded-md text-base font-medium'
+                                                )}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </Disclosure.Button> </Link>: ''
                                     ))}
                                 </div>
+                                {loggedIn ?
                                 <div className="pt-4 pb-3 border-t border-gray-700">
                                     <div className="flex items-center px-5">
                                         <div className="flex-shrink-0">
@@ -189,7 +203,7 @@ export default function Navbar() {
                                             </Disclosure.Button>
                                         ))}
                                     </div>
-                                </div>
+                                </div>:''}
                             </Disclosure.Panel>
                         </>
                     )}
