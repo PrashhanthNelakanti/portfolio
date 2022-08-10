b0a4f953abc2b22756569dd2a3a35964e57f8697
prashhanthsoujanya <prashhanth.nelakanti@gmail.com>
2022-08-10 10:54:05 +0530

    dynamic profile pdf

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,7 @@
 import Marquee from "react-fast-marquee";
 import Footer from "../components/Footer";
 const Home = () => {
+  console.log(process.env.FREE_LANCER)
   return (
     <div>
       <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-18">
